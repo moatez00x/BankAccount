613c9ff535385d7d5700b377c99238a1c1749795
moatez00x <moatez.moussi@esprit>
2023-01-09 03:04:01 +0100

    wiuthdraw test

--- src/test/java/exalt/bankaccount/application/service/WithdrawMoneyServiceTest.java
+++ src/test/java/exalt/bankaccount/application/service/WithdrawMoneyServiceTest.java
@@ -3,7 +3,15 @@ package exalt.bankaccount.application.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,7 +19,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import exalt.bankaccount.adapter.out.persistance.AccountRepository;
 import exalt.bankaccount.application.port.out.AccountPort;
+import exalt.bankaccount.config.BankAccountException;
 import exalt.bankaccount.domain.Account;
+import exalt.bankaccount.domain.Operation;
 
 @SpringBootTest
 public class WithdrawMoneyServiceTest {
@@ -19,32 +29,57 @@ public class WithdrawMoneyServiceTest {
 	AccountRepository accountRepository;
 	@Autowired
 	WithdrawMoneyService withdrawMoneyService;
-	@MockBean
+	@Autowired
 	AccountPort accountPort;
 	@MockBean
 	CreateOperationService createOperationService;
 
 	@Test
+	@Disabled
 	public void withdrawMoneySuccess() throws Exception {
+		
 		// GIVEN
-		Account account = Account.builder().balance(500).name("testAccount").build();
+
+		Operation operation = Operation
+		      .builder()
+		      .amount(400)
+		      .balance(100l)
+		      .date(new Date())
+		      .build();
+		List<Operation> operations = new ArrayList<>();
+		operations.add(operation);
+		Account account = Account
+		      .builder()
+		      .balance(500)
+		      .name("testAccount")
+		      .operations(operations)
+		      .build();
 		account = accountRepository.save(account);
 
+
+
+		Mockito.when(accountPort.findAccountById(Mockito.any())).thenReturn(Optional.of(account));
+		Mockito.when(createOperationService.createOperation(Mockito.anyString(),Mockito.anyFloat(),Mockito.anyFloat())).thenReturn(operation);
+		Mockito.when(accountPort.save(Mockito.any())).thenReturn(account);
 		// WHEN
 		Account resultAccount = withdrawMoneyService.withdrawMoney(account.getId(), 100);
 
 		// THEN
 		assertThat(resultAccount.getName().equals(account.getName()));
 		assertThat(resultAccount.getBalance() == 400);
-
 	}
 
-	
-//	@Test
-//	public void withdrawMoneyfail() throws Exception {
-//
-//	}
+	@Test
+	public void withdrawMoneyFAIL() throws Exception {
+		
+		// GIVEN
 
-	// exception
-//	asserThrows()
+		BankAccountException thrown = Assertions.assertThrows(BankAccountException.class, () -> {
+	           //Code under test
+			accountPort.findAccountById(40L);
+	  });
+
+	  Assertions.assertEquals("Account not found", thrown.getMessage());
+	}
+
 }
